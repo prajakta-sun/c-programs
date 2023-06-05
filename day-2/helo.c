cde61fc752b52eff32332bc914efd16a0f212760
prajakta-sun <prajaktagaware97@gmail.com>
2023-06-05 11:43:45 +0530

    switch case edited

--- day-2/helo.c
+++ day-2/helo.c
@@ -1,26 +1,25 @@
 #include<stdio.h>
 
-{
  int choice;
     int num1,num2,res;
-     while ((choice=menu_list()) !=0);
-           
+while ((choice=menu_list()) !=0)
+{          
     
         take_input (&num1,&num2);
-switch (choice)
-    {
-case 1:
-     res =addition(num1,num2);
-break;
+     switch (choice)
+     {
+          case 1:
+               res =addition(num1,num2);
+          break;
 
-case 2:
-     res =subtration(num1,num2);
-break;
-case 3:
-     res =multiplication(num1,num2);
-break;
-case 4:
-     res =division(num1,num2);
- 
-    }
-}    
+          case 2:
+               res =subtration(num1,num2);
+          break;
+          case 3:
+               res =multiplication(num1,num2);
+          break;
+          case 4:
+               res =division(num1,num2);
+     
+     } 
+}
