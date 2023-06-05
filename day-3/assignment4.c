4d147b40bd0249f530631a1b3814313b1d371ffb
prajakta-sun <prajaktagaware97@gmail.com>
2023-06-05 11:33:36 +0530

    variable change

---
+++ day-3/assignment4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int dep_no,dep_name,dsg_code,desg;
+int employee_id;
+switch (employee_id)
+{
+case 1:
+      printf("dept_no");
+      
+    break;
+
+default:
+    break;
+}
+
+
