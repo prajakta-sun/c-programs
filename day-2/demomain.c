8c23cf326169b36b03a7ad250ccde0491a6e6199
prajakta-sun <prajaktagaware97@gmail.com>
2023-06-05 11:00:20 +0530

    program shift

---
+++ day-2/demomain.c
@@ -0,0 +1,15 @@
+#include "helo.c"
+#include "helo.h"
+
+    int menu_list();
+{
+    void take_input (int*,int*);
+    int_addition( int ,int);
+    int_subtraction(int,int);
+    int_multiplication(int,int);
+    int_division(int,int);
+    void print_result(int);
+
+
+ return 0;
+}
