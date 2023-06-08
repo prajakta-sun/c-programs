ad7816fd72ca0d010d36d7241345bdaabfd8021e
prajakta-sun <prajaktagaware97@gmail.com>
2023-06-08 20:59:29 +0530

    signup

--- day-5/assign4.c
+++ day-5/assign4.c
@@ -12,33 +12,53 @@
 #include <string.h> 
 #include <conio.h> 
  
-
+struct user{
+char fullname[30];
+char email[30];
+char username[20]; 
+char password[12]; 
+ char stelen[20];
+};
  
+ 
+void takeinput(char ch[30]){
+fgets(ch,50,stdin);
+
+}
+ void generateusername(char email[30],char username[20]){
+ for(int i=0;i<strlen(email);i++){
+    if(email[i]='@')break;
+    else username[i]=email[i];
+ }}
 int main() 
-{ 
+{    int choice;
     char username[20]; 
     char password[12]; 
- 
- 
-    printf("Enter your username:\n"); 
+   
+     printf("choice \n");
+     scanf("%d",&choice);
+     printf("1.signup");
+     printf("2.login \n");
+     printf("3.exit \n");
+     printf("Enter your username:\n"); 
     scanf("%s",&username); 
- 
-    printf("Enter your password:\n"); 
-    scanf("%s",&password); 
- 
-    if(strcmp(username,"prajaktagaware97@gmail.com")==0){ 
-        if(strcmp(password,"12341234")==0){ 
- 
-        printf("\nWelcome.Login Success!"); 
- 
- 
-        }else{ 
-    printf("\nwrong password"); 
-} 
-    }else{ 
-    printf("\nexit"); 
-} 
- 
-    return 0; 
- 
-} 
+    
+        printf("Enter your password:\n"); 
+        scanf("%s",&password); 
+    
+        if(strcmp(username,"prajaktagaware97@gmail.com")==0){ 
+            if(strcmp(password,"12341234")==0){ 
+    
+            printf("\nWelcome.Login Success!"); 
+    
+    
+            }
+            else{ printf("\nwrong password"); 
+            } 
+        }
+        else{ printf("\nexit"); 
+        } 
+    
+        return 0; 
+    
+    } 
