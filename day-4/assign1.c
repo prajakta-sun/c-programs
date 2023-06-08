77454d186ab52c8f0f96f7bef5e78f171ed517d1
prajakta-sun <prajaktagaware97@gmail.com>
2023-06-08 10:07:44 +0530

    book lib ass

--- day-4/assign1.c
+++ day-4/assign1.c
@@ -1,84 +1,192 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-struct library
-{
-   char title[30];
-   char author[30];
-   
+
+// Create a program in C that simulates a simple library catalog. Use a switch
+// case to handle menu options such as adding a book,searching for a book by title or
+// author, displaying all books, and removing a book from the catalog. Implement these
+// operations using an array of structures where each structure represents a book with
+// attributes like title, author, and ISBN
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BOOKS_FILE  "books.db"
+
+struct book {
+    int  ISBN;
+    char name[40];
+    char author_name[20];
+    
 };
-int main()
-{
-   struct library lib[10];
-   //char title[20];
-   int a,b,books,i;
-   a=b=books= 0;
-   while(b!=4)
-   {
-      printf(" 1. Adding book \n");
-      printf("2.Display  book \n");
-      printf ("3.search a book by title :author  \n ");
-       printf("4.del book \n");
-      scanf("%d",&b);
-   }
-      switch (b){
-         /* Add book */
-         case 1:
-             
-             {
-            printf ("Enter title = ");
-            scanf ("%s",lib[books].title);
 
-             for(i=0; i<4; i++)
-            printf ("Enter author name = ");
-            scanf ("%s",lib[books].author);
-             }
+void accept_book(struct book *b) {
+    printf("enter ISBN: ");
+    scanf("%d%*c", &b->ISBN);
+    printf("enter book name: ");
+    scanf("%[^\n]", b->name);
+    printf("enter author name: ");
+    scanf("%[^\n]", b->author_name);
+}
 
-            // printf ("Enter pages = ");
-            printf(" exit");
+void print_book(struct book *b) {
+    printf("ISBN = %d\nname = %s\nauthor_name =%s\n", b->ISBN, b->name, b->author_name);
+}
+//add book
+void add_book() {
+    FILE *fp;
+    struct book b;
+    accept_book(&b);
+  
+    fp = fopen(BOOKS_FILE, "ab");
+    if(fp == NULL) {
+        perror("fopen() failed");
+        exit(1);
+    }
+    
+    fwrite(&b, sizeof(struct book), 1, fp);
+    printf("New book saved.\n");
+    fclose(fp);
+}
+//display book
+void display_all_books() {
+    FILE *fp;
+    struct book b;
+    fp = fopen(BOOKS_FILE, "rb");
+    if(fp == NULL) {
+        perror("fopen() failed");
+        exit(1);
+    }    
+    while(fread(&b, sizeof(struct book), 1, fp) > 0) {
+        print_book(&b);
+    }
+    fclose(fp);
+}
+//find book by name
+void find_book_by_name() {
+    char name[40];
+    FILE *fp;
+    struct book b;
+    int found = 0;
+
+    printf("enter book name to find: ");
+    scanf("%*c%[^\n]", name);
+    strupr(name);
+    
+    fp = fopen(BOOKS_FILE, "rb");
+    if(fp == NULL) {
+        perror("fopen() failed");
+        exit(1);
+    }
+    
+    while( fread(&b, sizeof(struct book), 1, fp) > 0 ) {
+        
+        strupr(b.name);
+        if(strstr(b.name, name) != NULL) {
+            found = 1;
+            print_book(&b);
+        }
+    } 
+    if(!found)  
+        printf("No matching book found.\n");
+    
+    fclose(fp);
+}
+    //find book by author
+    void find_book_by_author() {
+    char name[40];
+    FILE *fp;
+    struct book b;
+    int found = 0;
+    
+    printf("enter author name to find: ");
+    scanf("%*c%[^\n]", name);
+    strupr(name);
+    // 
+    fp = fopen(BOOKS_FILE, "rb");
+    if(fp == NULL) {
+        perror("fopen() failed");
+        exit(1);
+    }
+    
+    while( fread(&b, sizeof(struct book), 1, fp) > 0 ) {
+      
+        strupr(b.name);
+        if(strstr(b.name, name) != NULL) {
+            found = 1;
+            print_book(&b);
+        }
+    } 
+    if(!found)  
+        printf("No matching book found.\n");
+    
+    fclose(fp);
+}
+
+      //delete book
+    void delete_book() {
+    int id, found = 0;
+    FILE *fp, *fd;
+    struct book b;
+   
+    printf("enter id of book to be modified: ");
+    scanf("%d%*c", &id);
+    
+    fp = fopen(BOOKS_FILE, "rb");
+    if(fp == NULL) {
+        perror("fopen() failed");
+        exit(1);
+    }
+   
+    fd = fopen("temp.db", "wb");
+    if(fd == NULL) {
+        perror("fopen() failed");
+        exit(1);
+    }
+    
+    while(fread(&b, sizeof(struct book), 1, fp) > 0) {
+
+        if(b.ISBN != id) {
            
-             break;
+            fwrite(&b, sizeof(struct book), 1, fd);
+        }
+        else
+            found = 1;
+    } 
+    fclose(fd);
+    
+    fclose(fp);
+    if(!found)
+        printf("No book found!\n");
+    else
+        printf("Book deleted!\n");
+    
+    remove(BOOKS_FILE);
+    
+    rename("temp.db", BOOKS_FILE);
+}
 
-         case 2://display book
-               printf("displaying information: \n");
-                for(i=0; i<4; i++)
-            
-            printf ("\t \nbook name = %s",lib[books].title);
-            puts(lib[i].title);
-            printf ("\t author name = %s",lib[books].author);
-               
-           break;
-         case 3://search by title
-
-            printf("search a book by title \n");
-            scanf("\n %d",&books);
-            for(i=0; i<4; i++)
-         {
-               if(books=lib[i].title)
-            {
-               printf("title: %s n",lib[i].title);
-
-               printf("title: %f n",lib[i].title);
-            }
-            
-            break;
-         }
-            
-         case 4://search by author
-         
-          printf("search a book by author\n");
-            scanf("\n %d",&books);
-            for(i=0; i<4; i++){
-               if(books=lib[i].title)
-            {
-               printf("title: %s n",lib[i].title);
-               printf("title: %f n",lib[i].title);
-            }
-         case 5:
-                //del book
-            printf("remove book");
-            break;
-            }
-      }
-   return 0;
+int main() {
+    int choice;
+    do {
+        printf("\n0. exit\n1. add new book\n2. display all books\n3. find book by name\n4. find book by author\n5. delete book\nenter choice: ");
+        scanf("%d", &choice);
+        switch(choice) {
+            case 1:
+                add_book();
+                break;
+            case 2:
+                display_all_books();
+                break;
+            case 3:
+                find_book_by_name();
+                break;
+            case 4:
+                find_book_by_author();
+                break;
+            case 5:
+                delete_book();
+                break;
+        }
+    } while(choice != 0);
+    return 0;
 }
--- day-4/struct.c
+++ day-4/struct.c
@@ -1,64 +1,80 @@
-//#include<stdio.h>
 
-// int main()
-
-
-// struct date {
-
-//     int i, day,year;
-//     char month[20];
-  
-
-
-   #include <dos.h>
-#include <stdio.h>
- 
+ #include<stdio.h>
+ #include<conio.h>
+#include<time.h>
 int main()
 {
-    struct date  {
+    time_t t;
+    t = time(NULL);
+    struct tm tm = *localtime(&t);
+     printf("Current Date: %d-%d-%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year+1900);
 
-        int d,m,y;
-        char;
-int dt;
-    };
-
-    // This function is used to get
-    // system's current date
-    getdate;
- {
-    printf("System's current date\n");
-    printf("%d/%d/%d",
-           dt.da_day,
-           dt.da_mon,
-           dt.da_year);
- }
- {
-    printf("Enter date in the format (date month year)\n");
-    scanf("%d%d%d", &dt.da_day, &dt.da_mon, &dt.da_year);
- }
-    // This function is used to add date
-    // system's current date
-    //add date(&dt);
-    void addDays(int d, int m, int y, int x)
-  
-
-   {printf("System's new date (dd/mm/yyyy)\n");
-        printf("%d%d%d", dt.da_day, dt.da_mon, dt.da_year);
-        scanf("%d%*c",&new date);
-        break;
-   }
-printf("enter first date(dd/mm/yyyy)");
-scanf("%d%d%d”,&d.day,&d.month,&d.year);
-printf("enter second date(dd/mm/yyyy):");
-scanf("%d%d%d", &d.day,&d.month,&d.year);
-
-//if((d1.day==d2.day)&&(d1.month==d2.month)&&(d1.year==d2.year))
-//printf(“nEQUAL”);
-//else
-//printf(“nUNEQUAL”);
-
-
-}
-
-return 0;
+  int day=0;
+  int month=0;
+  int year=0;
+  int n=0;
+  //printf("Enter the date\n");
+  //scanf("%d/%d/%d",&day,&month,&year);
+  if(month>=1&&month<=12)
+  {
+    if(month==1||month==3||month==5||month==7||month==8||month==10||month==12)
+    {
+      if(day>=1&&day<=31)
+      {
+        printf("%d/%d/%d\n",day,month,year);
+      }else{
+        printf("Invalid date");
+      }
+    }
+    else if(month==4||month==6||month==9||month==11)
+    {
+      if(day>=1&&day<=30)
+      {
+        printf("%d/%d/%d\n",day,month,year);
+      }else{
+        printf("Invalid date");
+      }
+    }
+    else if(month==2)
+    {
+      if(year%4==0)
+      {
+        if(year%100==0)
+        {
+          if(year%400==0)
+          {
+            if(day>=1&&day<=29)
+            {
+              printf("%d/%d/%d\n",day,month,year);
+            }else{
+              printf("Invalid date.\n");
+            }
+          }
+          else if(day>=1&&day<=28)
+          {
+            printf("%d/%d/%d\n",day,month,year);
+          }else{
+            printf("Invalid date");
+          }
+        }
+        else if(day>=1&&day<=29)
+        {
+          printf("%d/%d/%d\n",day,month,year);
+        }else{
+          printf("Invalid date");
+        }
+      }
+      else if(day>=1&&day<=28)
+      {
+        printf("%d/%d/%d\n",day,month,year);
+      }else{
+        printf("Invalid date");
+      }
+    }
+  }
+  else{
+   printf("Enter number of days\n");
+  scanf("%d",&n);
+  return 0;
+  }
 }
